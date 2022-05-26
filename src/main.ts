5ca8ee57e4b99695d918cd1cd3286a4319b70207
Alexei Pannikov <alexeipannikov@outlook.com>
2022-05-26 19:01:47 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import {loadFonts} from "./plugins/webfontloader";
+import {loadFonts} from "./plugins/webfontloader";
 import vuetify from "./plugins/vuetify";
 
 const app = createApp(App)
-// loadFonts()
+loadFonts()
 
 app.use(vuetify)
 app.mount('#app')
