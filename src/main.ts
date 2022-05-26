bedd3a2c527ed1cf3cf0b8195971d6c0b0c8389a
Alexei Pannikov <alexeipannikov@outlook.com>
2022-05-26 18:49:48 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {loadFonts} from "./plugins/webfontloader";
 import vuetify from "./plugins/vuetify";
 
 const app = createApp(App)
-await loadFonts()
+loadFonts()
 
 app.use(vuetify)
 app.mount('#app')
