1074d6eb290faf769dbd3d92d8d9176c1e56362d
Alexei Pannikov <alexeipannikov@outlook.com>
2022-05-30 21:59:08 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { ViteAliases } from 'vite-aliases'
 import vuetify from '@vuetify/vite-plugin'
+import {ViteAliases} from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteAliases(),     vuetify({
-    autoImport: true,
-  }),]
+    plugins: [
+        vue(),
+        ViteAliases(),
+        vuetify({
+            autoImport: true,
+        }),
+    ],
+    define: {'process.env': {}},
+    build: {
+        outDir: 'dist'
+    },
+
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+        hmr: true
+    }
 })
